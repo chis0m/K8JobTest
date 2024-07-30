4dea23e493ede8c2f5a5ac669c54c413e727b0b3
Chisom Ejim <ejimchisom@gmail.com>
2024-07-30 10:07:58 +0100

    add efk

--- k8/installtions/loki/delete.sh
+++ k8/installtions/loki/delete.sh
@@ -5,6 +5,8 @@ SCRIPT_DIR=$(dirname "$(realpath "${BASH_SOURCE[0]}")")
 echo "The script directory is: $SCRIPT_DIR"
 
 # Delete Helm release
-helm uninstall loki
+helm uninstall grafana -n grafana
+helm uninstall loki -n loki
+helm uninstall promtail -n loki
 
 echo "Cleanup completed successfully."
